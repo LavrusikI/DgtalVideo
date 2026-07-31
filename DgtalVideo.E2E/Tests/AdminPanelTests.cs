30d5ffd25dd0281389d226b9c857cf6d4738381f
Ira <iriplay1995@gmail.com>
2026-07-31 15:21:46 +0300

    Add Folder E2E Tests

---
+++ DgtalVideo.E2E/Tests/AdminPanelTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DgtalVideo.Tests.E2E.Tests
+{
+    public class AdminPanelTests
+    {
+        private IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+        }
+        [Test]
+        public void AddMovie()
+        {
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)_driver;
+            _driver.Navigate().GoToUrl("https://localhost:7134/Auth/Login");
+            _driver.FindElement(By.XPath("//input[@id='Login']")).SendKeys("admin");
+            _driver.FindElement(By.XPath("//input[@id='Password']")).SendKeys("admin");
+            _driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+            _driver.Navigate().GoToUrl("https://localhost:7134/AdminPanel/AdminPanel");
+            executor.ExecuteScript("window.scrollBy(0, 1000);");
+            Thread.Sleep(500);
+            executor.ExecuteScript("arguments[0].click();", _driver.FindElement(By.XPath("//button[@id='togglePortfolioForm']")));
+            Thread.Sleep(500);
+            executor.ExecuteScript("arguments[0].click();", _driver.FindElement(By.XPath("//button[@data-close-form='portfolioCreateForm']")));
+            executor.ExecuteScript("arguments[0].click();", _driver.FindElement(By.XPath("//button[@id='togglePortfolioForm']")));
+            Thread.Sleep(500); 
+            _driver.FindElement(By.XPath("//input[@id='NewPortfolio_Title']")).SendKeys("Промо для альфа банка");
+            _driver.FindElement(By.XPath("//input[@id='NewPortfolio_Category']")).SendKeys("Промо");
+            _driver.FindElement(By.XPath("//textarea[@id='NewPortfolio_Description']")).SendKeys("Промо для альфа банка");
+            _driver.FindElement(By.XPath("//input[@id='NewPortfolio_UrlMovie']")).SendKeys("/videos/alfa-bank.mp4");
+            executor.ExecuteScript("arguments[0].click();", _driver.FindElement(By.XPath("//button[contains(text(),'Сохранить работу')]")));
+            Thread.Sleep(2000);
+            executor.ExecuteScript("window.scrollBy(0, 1500);");
+            Thread.Sleep(500);
+            executor.ExecuteScript("arguments[0].click();", _driver.FindElement(By.XPath("//button[@id='toggleReviewForm']")));
+            Thread.Sleep(500);
+            _driver.FindElement(By.XPath("//input[@id='NewReview_Name']")).SendKeys("Валерия С");
+            _driver.FindElement(By.XPath("//input[@id='NewReview_ShortDescription']")).SendKeys("Видеограф");
+            _driver.FindElement(By.XPath("//input[@id='NewReview_ShortDescription']")).SendKeys("Делают очень качественно");
+            executor.ExecuteScript("arguments[0].click();", _driver.FindElement(By.XPath("//button[contains(text(),'Сохранить отзыв')]")));
+            Thread.Sleep(2000);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+
+    }
+}
---
+++ DgtalVideo.E2E/Tests/AuthRegistrationTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DgtalVideo.Tests.E2E.Tests
+{
+    public class AuthRegistrationTests
+    {
+        private IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Login()
+        {
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)_driver;
+            _driver.Navigate().GoToUrl("https://localhost:7134/Auth/Login");
+            var loginInput = _driver.FindElement(By.CssSelector("#Login"));
+            loginInput.SendKeys("admin");
+            var passwordInput = _driver.FindElement(By.CssSelector("#Password"));
+            passwordInput.SendKeys("admin");
+            _driver.FindElement(By.CssSelector(".admin-button")).Click();
+            _driver.FindElement(By.XPath("//a[@class='site-nav__link']")).Click();
+            _driver.FindElement(By.CssSelector(".registration-link-style")).Click();
+            _driver.FindElement(By.CssSelector("#Login")).SendKeys("admin");
+            _driver.FindElement(By.CssSelector("#Password")).SendKeys("admin");
+            _driver.FindElement(By.CssSelector("#Name")).SendKeys("admin");
+            Thread.Sleep(500);
+            _driver.FindElement(By.CssSelector("#MobilePhone")).SendKeys("+375664447788");
+            Thread.Sleep(500);
+            executor.ExecuteScript("arguments[0].click();", _driver.FindElement(By.XPath("//button[@type='submit']")));
+            Thread.Sleep(500);
+            _driver.FindElement(By.CssSelector("#Login")).SendKeys("user39");
+            _driver.FindElement(By.CssSelector("#Password")).SendKeys("user39");
+            _driver.FindElement(By.CssSelector("#Name")).SendKeys("user39");
+            Thread.Sleep(500);
+            executor.ExecuteScript("arguments[0].click();", _driver.FindElement(By.XPath("//button[@type='submit']")));
+        }
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+    }
+}
